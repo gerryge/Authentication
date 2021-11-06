ab6e8e1a6006b5e921655aa9113c7ce2f0293c2f
Gerry Ge <99432281@qq.com>
2021-11-06 11:18:10 +0800

    Add comments

--- Basics/Controllers/HomeController.cs
+++ Basics/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,7 +39,7 @@ namespace Basics.Controllers
         {
             var gerryClaims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Name, "Bob", ClaimValueTypes.String),
                 new Claim(ClaimTypes.Email, "Bob@gerry.com"),
                 new Claim(ClaimTypes.DateOfBirth, "1/1/1990"),
                 new Claim(ClaimTypes.Role, "Admin"),
@@ -51,7 +55,17 @@ namespace Basics.Controllers
             var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
 
             var userPrincipal = new ClaimsPrincipal(new[] { gerryIdentity, licenseIdentity });
-
+            //Debug.Assert(userPrincipal.Identities.First() is WindowsIdentity,"IIdentity is not WindowsIdentity");
+            //IAuthenticationHandler
+            //IAuthenticationSignInHandler
+            //CookieAuthenticationHandler
+            //IAuthenticationRequestHandler
+            //IAuthenticationHandlerProvider提供的GetHandlerAsync方法，根据当前HttpContext上下文和认证方案名来提供对用的IAuthenticaionHandler
+            
+            //AuthenticationScheme的HandleType为我们提供所需的认证处理器类型，
+            //但是接下来的问题是如何根据认证方案名称得到对应的AuthenticationScheme对象，
+            //这个问题需要借助IAuthenticationSchemeProvider对象来解决，
+            //它不仅能够通过GetSchemeAsync(string name)帮助我们获取所需的方案，还能通过AddScheme注册方案
             HttpContext.SignInAsync(userPrincipal);
             return RedirectToAction("Index");
         }
